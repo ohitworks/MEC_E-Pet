b73a60e224a27dbd3f7a47092a67f14e3ea2d684
1cal <1073259641@qq.com>
2023-05-05 15:39:15 +0800

    chore: Ultrasonic drive test

--- test/Ultrasonic_drive_test/Ultrasonic_drive_test.ino
+++ test/Ultrasonic_drive_test/Ultrasonic_drive_test.ino
@@ -1,5 +1,5 @@
-const int TrigPin=8;           //Set pin 8 as the launching pin position
-const int EchoPin=9;           //Set pin 9 as the receiving pin position
+const int TrigPin=6;           //Set pin 6 as the launching pin position
+const int EchoPin=7;           //Set pin 7 as the receiving pin position
 float cm;                 
 
 void setup() {
@@ -10,15 +10,20 @@ pinMode(EchoPin,INPUT);
 
 void loop() {
 digitalWrite(7, LOW);
+
+
 digitalWrite(TrigPin,LOW);        //Send level pulses to the Trigpin pins in low and high order
 delayMicroseconds(2);             //Delay time
 digitalWrite(TrigPin,HIGH);
 delayMicroseconds(10);
 digitalWrite(TrigPin,LOW);
 cm=pulseIn(EchoPin,HIGH)/58.0;    //Read the pulse width and convert it to centimeters
-if(cm<=40){
-  digitalWrite(7,HIGN);           //The distance is less than 40cm and the 7 pin changes from low to high
+Serial.print(cm);
+if(cm<=40.0){
+  digitalWrite(7, HIGH);
 }
+Serial.println(); 
+
 delay(1000);
 }
 
